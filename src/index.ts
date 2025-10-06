25eaae9a5d34612472cd9361e031fc5292948603
google-colabtools <fcmoabi@gmail.com>
2025-10-05 21:03:50 -0300

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -915,7 +915,7 @@ export class MicrosoftRewardsBot {
 
         const afterPointAmount = await this.browser.func.getCurrentPoints()
 
-        log(this.isMobile, 'MAIN-POINTS', `The script collected ${afterPointAmount - initialPoints} points today`)
+        log(this.isMobile, 'MAIN-POINTS', `The script collected ${afterPointAmount - initialPoints} points today: Current total: ${afterPointAmount} points.`)
 
         // Close mobile browser
         await this.browser.func.closeBrowser(browser, account.email)
